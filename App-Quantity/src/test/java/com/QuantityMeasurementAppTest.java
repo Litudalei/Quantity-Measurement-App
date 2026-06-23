e49a9d14ec25cbde5277d9fd48d5c98cf6853c8f
Litudalei <gitiranjan815@gmail.com>
2026-06-23 16:08:43 +0530

    Uc1 completed

---
+++ App-Quantity/src/test/java/com/QuantityMeasurementAppTest.java
@@ -0,0 +1,59 @@
+package com;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QuantityMeasurementAppTest {
+
+
+    @Test
+    public void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet feet1 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        QuantityMeasurementApp.Feet feet2 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(feet1.equals(feet2));
+    }
+
+    @Test
+    public void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet feet1 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        QuantityMeasurementApp.Feet feet2 =
+                new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(feet1.equals(feet2));
+    }
+
+    @Test
+    public void testEquality_NullComparison() {
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(feet.equals(null));
+    }
+
+    @Test
+    public void testEquality_NonNumericInput() {
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        String value = "ABC";
+
+        assertFalse(feet.equals(value));
+    }
+
+    @Test
+    public void testEquality_SameReference() {
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(feet.equals(feet));
+    }
+}
